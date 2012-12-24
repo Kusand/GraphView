f082dc8ff2ff52e13fdbb0a09ac2fd040b0b512c
Eric Schrag <eschrag@gmail.com>
2012-12-24 17:24:08 -0500

    Add new styled demo view.

--- sample/src/com/kusand/graphview/GraphViewDemo.java
+++ sample/src/com/kusand/graphview/GraphViewDemo.java
@@ -1,36 +1,11 @@
 package com.kusand.graphview;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.ListActivity;
 
-import com.kusand.graphview.GraphView.GraphViewData;
+public class GraphViewDemo extends ListActivity {
 
-/**
- * GraphViewDemo creates some dummy data to demonstrate the GraphView component.
- *
- * IMPORTANT: For examples take a look at GraphView-Demos (https://github.com/jjoe64/GraphView-Demos)
- *
- * Copyright (C) 2011 Jonas Gehring
- * Licensed under the GNU Lesser General Public License (LGPL)
- * http://www.gnu.org/licenses/lgpl.html
- */
-public class GraphViewDemo extends Activity {
-	/**
-	 * @param savedInstanceState
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-        setContentView(R.layout.default_line_graph);
-		LineGraphView graphView = (LineGraphView) findViewById(R.id.sample_line_graph);
-        graphView.setTitle("GraphViewDemo");
-		graphView.addSeries(new GraphViewSeries(new GraphViewData[] {
-				new GraphViewData(1, 2.0d)
-				, new GraphViewData(2, 1.5d)
-				, new GraphViewData(2.5, 3.0d)
-				, new GraphViewData(3, 2.5d)
-				, new GraphViewData(4, 1.0d)
-				, new GraphViewData(5, 3.0d)
-		}));
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
---
+++ sample/src/com/kusand/graphview/LineGraphViewDemo.java
@@ -0,0 +1,36 @@
+package com.kusand.graphview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kusand.graphview.GraphView.GraphViewData;
+
+/**
+ * LineGraphViewDemo creates some dummy data to demonstrate the GraphView component.
+ *
+ * IMPORTANT: For examples take a look at GraphView-Demos (https://github.com/jjoe64/GraphView-Demos)
+ *
+ * Copyright (C) 2011 Jonas Gehring
+ * Licensed under the GNU Lesser General Public License (LGPL)
+ * http://www.gnu.org/licenses/lgpl.html
+ */
+public class LineGraphViewDemo extends Activity {
+	/**
+	 * @param savedInstanceState
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.default_line_graph);
+		LineGraphView graphView = (LineGraphView) findViewById(R.id.sample_line_graph);
+        graphView.setTitle("LineGraphViewDemo");
+		graphView.addSeries(new GraphViewSeries(new GraphViewData[] {
+				new GraphViewData(1, 2.0d)
+				, new GraphViewData(2, 1.5d)
+				, new GraphViewData(2.5, 3.0d)
+				, new GraphViewData(3, 2.5d)
+				, new GraphViewData(4, 1.0d)
+				, new GraphViewData(5, 3.0d)
+		}));
+	}
+}
